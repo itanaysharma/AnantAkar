3ed319230b3c5d948b5ad4d96a5956fe1612c198
itanaysharma <thisistanaysharma@gmail.com>
2023-08-03 15:19:42 +0530

    home page is in progress

--- src/App.js
+++ src/App.js
@@ -15,9 +15,7 @@ const MyComponent = () => {
           <div className="line h-0.5 w-5 my-1 bg-white"></div>
           <div className="line h-0.5 w-5 my-1 bg-white"></div>
         </div>
-        <div className="pattern text-white text-sm font-mono cursor-pointer">
-          Pattern
-        </div>
+        <div className="pattern text-white text-sm font-mono">Pattern</div>
         <div className="text-white text-base cursor-pointer">
           <span className="text-red-500 text-lg font-mono">A</span>
           <span className="text-red-500 text-lg font-mono">A</span>
@@ -29,18 +27,44 @@ const MyComponent = () => {
           User
         </div>
       </div>
-      <div className="hidden md:block ">
-        <nav className="mid-navbar bg-black flex justify-around">
-          <div className="text-white text-lg cursor-pointer">
-            <span className="text-red-500 text-3xl font-mono">A</span>nant
-            <span className="text-red-500 text-3xl font-mono">A</span>akar
+      <div className="hidden md:flex justify-between">
+        <div className="h-screen text-white text-base bg-black flex flex-col justify-between">
+          <div className="font-extrabold xl:text-xl 2xl:text-2xl p-5 md:pl-5 md:pr-20 xl:pl-10 xl:pr-32 2xl:pr-44">
+            <span className="text-red-500 text-3xl xl:text-4xl 2xl:text-5xl font-mono">
+              A
+            </span>
+            nanat
+            <span className="text-red-500 text-3xl xl:text-4xl 2xl:text-5xl font-mono">
+              A
+            </span>
+            akar
           </div>
-          <div className="text-white font-mono flex justify-between">
-            <div className="px-16 xl:px-24">Pattern</div>
-            <div className="px-16 xl:px-24">Orders</div>
-            <div className="px-16 xl:px-24">User</div>
+          <div className=" mb-20 text-xl font-mono">
+            <div className=" cursor-pointer">Clothing</div>
+            <div className=" cursor-pointer">Clothing</div>
+            <div className=" cursor-pointer">Clothing</div>
+            <div className=" cursor-pointer">Clothing</div>
           </div>
-        </nav>
+          <div className="pb-16">
+            <div>Return</div>
+            <div>Return</div>
+            <div>Return</div>
+          </div>
+        </div>
+        <div className="flex-col font-mono font-bold py-4 xl:py-6 2xl:py-7">
+          <div className="flex">
+            <div className="md:px-16 xl:px-20 xl:text-lg 2xl:text-xl">
+              <span className="cursor-pointer">Pattern</span>
+            </div>
+            <div className="md:px-16 xl:px-20 xl:text-lg 2xl:text-xl">
+              <span className="cursor-pointer">Orders</span>
+            </div>
+            <div className="md:px-16 xl:px-20 xl:text-lg 2xl:text-xl">
+              <span className="cursor-pointer">User</span>
+            </div>
+          </div>
+          <div>Content</div>
+        </div>
       </div>
     </>
   );
